fc75bee6a1d4766b784456d475ed26b3bf558298
Rohan Saklani <92996444+itsrohan2003@users.noreply.github.com>
2023-07-06 13:55:00 +0530

    integrate map next

---
+++ newRide.js
@@ -0,0 +1,93 @@
+
+  let map;
+  let currentLocationMarker;
+  let destinationMarker;
+  let geocoder;
+
+  function initMap() {
+    const mapContainer = document.getElementById("map");
+    const mapContainerHeight = mapContainer.offsetHeight;
+
+    if (mapContainerHeight > 0) {
+      // Initialize the map centered at the specific location
+      map = L.map("map").setView([26.8439, 75.5652], 12);
+
+      // Add the OpenStreetMap tiles to the map
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // Add markers for current location and destination
+      currentLocationMarker = L.marker([26.8439, 75.5652])
+        .addTo(map)
+        .bindPopup("Current Location");
+      destinationMarker = L.marker([0, 0])
+        .addTo(map)
+        .bindPopup("Destination");
+
+      geocoder = L.Control.geocoder({
+        defaultMarkGeocode: false,
+        placeholder: "Search...",
+        geocoder: L.Control.Geocoder.nominatim(),
+      })
+        .on("markgeocode", function (e) {
+          const latlng = e.geocode.center;
+          if (e.target.options.id === "current-location") {
+            currentLocationMarker.setLatLng(latlng);
+            map.panTo(latlng);
+          } else if (e.target.options.id === "destination") {
+            destinationMarker.setLatLng(latlng);
+            map.panTo(latlng);
+          }
+        })
+        .addTo(map);
+
+      // Update the marker positions when the input fields change
+      document
+        .getElementById("current-location")
+        .addEventListener("input", updateCurrentLocationMarker);
+      document
+        .getElementById("destination")
+        .addEventListener("input", updateDestinationMarker);
+    }
+  }
+
+  function updateCurrentLocationMarker() {
+    const currentLocation = document.getElementById("current-location").value;
+    geocoder.geocode(currentLocation, function (results) {
+      if (results.length > 0) {
+        const latlng = results[0].center;
+        currentLocationMarker.setLatLng(latlng);
+        map.panTo(latlng);
+      }
+    });
+  }
+
+  function updateDestinationMarker() {
+    const destination = document.getElementById("destination").value;
+    geocoder.geocode(destination, function (results) {
+      if (results.length > 0) {
+        const latlng = results[0].center;
+        destinationMarker.setLatLng(latlng);
+        map.panTo(latlng);
+      }
+    });
+  }
+
+  function setLocation(inputId) {
+    const location = document.getElementById(inputId).value;
+    geocoder.geocode(location, function (results) {
+      if (results.length > 0) {
+        const latlng = results[0].center;
+        if (inputId === "current-location") {
+          currentLocationMarker.setLatLng(latlng).update();
+          map.panTo(latlng);
+        } else if (inputId === "destination") {
+          destinationMarker.setLatLng(latlng).update();
+          map.panTo(latlng);
+        }
+      }
+    });
+  }
+
