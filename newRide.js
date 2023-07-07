ac024b0f410862d2560e5540523fb7c78a142686
Rohan Saklani <92996444+itsrohan2003@users.noreply.github.com>
2023-07-07 16:15:27 +0530

    no change

--- newRide.js
+++ newRide.js
@@ -4,138 +4,125 @@ let destinationMarker;
 let geocoder;
 
 function initMap() {
-    const mapContainer = document.getElementById("map");
-    const mapContainerHeight = mapContainer.offsetHeight;
-  
-    if (mapContainerHeight > 0) {
-      // Initialize the map centered at the specific location
-      map = L.map("map").setView([26.8439, 75.5652], 12);
-  
-      // Add the OpenStreetMap tiles to the map
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-      }).addTo(map);
-  
-      // Create markers for current location and destination
-      currentLocationMarker = L.marker([26.8439, 75.5652], { draggable: true })
-        .addTo(map)
-        .bindPopup("Current Location");
-  
-      destinationMarker = L.marker([26.8289, 75.8056], { draggable: true })
-        .addTo(map)
-        .bindPopup("Destination");
-  
-      // Initialize the geocoder
-      geocoder = L.Control.Geocoder.nominatim();
-  
-      // Update the marker positions when the input fields change
-      document
-        .getElementById("current-location")
-        .addEventListener("input", updateCurrentLocationMarker);
-      document
-        .getElementById("destination")
-        .addEventListener("input", updateDestinationMarker);
-  
-      // Print location names in console after marker dragend events
-      currentLocationMarker.on("dragend", function (event) {
-        const latlng = event.target.getLatLng();
-        reverseGeocode(latlng, function (result) {
-          console.log("Current Location: ", result);
-          sendGeocodedAddress(result, 'currentLocation');
-        }, 'currentLocation');
-      });
-  
-      destinationMarker.on("dragend", function (event) {
-        const latlng = event.target.getLatLng();
-        reverseGeocode(latlng, function (result) {
-          console.log("Destination: ", result);
-          sendGeocodedAddress(result, 'destination');
-        }, 'destination');
-      });
-    }
-  }
+  const mapContainer = document.getElementById("map");
+  const mapContainerHeight = mapContainer.offsetHeight;
 
-  
+  if (mapContainerHeight > 0) {
+    // Initialize the map centered at the specific location
+    map = L.map("map").setView([26.8439, 75.5652], 12);
+
+    // Add the OpenStreetMap tiles to the map
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Create markers for current location and destination
+    currentLocationMarker = L.marker([26.8439, 75.5652], { draggable: true })
+      .addTo(map)
+      .bindPopup("Current Location");
+
+    destinationMarker = L.marker([26.8289, 75.8056], { draggable: true })
+      .addTo(map)
+      .bindPopup("Destination");
+
+    // Initialize the geocoder
+    geocoder = L.Control.Geocoder.opencage('c986aaa28af74f65b90e997289e37903');
+
+    // Update the marker positions when the input fields change
+    document
+      .getElementById("current-location")
+      .addEventListener("input", updateCurrentLocationMarker);
+    document
+      .getElementById("destination")
+      .addEventListener("input", updateDestinationMarker);
+
+    // Print location names in console after marker dragend events
+    currentLocationMarker.on("dragend", function (event) {
+      const latlng = event.target.getLatLng();
+      reverseGeocode(latlng, function (result) {
+        console.log(`Current Location: ${result}`);
+        sendGeocodedAddress(result, 'current');
+      }, 'current');
+    });
+
+    destinationMarker.on("dragend", function (event) {
+      const latlng = event.target.getLatLng();
+      reverseGeocode(latlng, function (result) {
+        console.log(`Destination: ${result}`);
+        sendGeocodedAddress(result, 'destination');
+      }, 'destination');
+    });
+  }
+}
 
 function updateCurrentLocationMarker() {
-    const currentLocation = document.getElementById("current-location").value;
-    geocoder.geocode(currentLocation, function (results) {
-        if (results.length > 0) {
-            const latlng = results[0].center;
-            currentLocationMarker.setLatLng(latlng);
-            map.panTo(latlng);
-        }
-    });
-}
-
-function updateDestinationMarker() {
-    const destination = document.getElementById("destination").value;
-    geocoder.geocode(destination, function (results) {
-        if (results.length > 0) {
-            const latlng = results[0].center;
-            destinationMarker.setLatLng(latlng);
-            map.panTo(latlng);
-        }
-    });
-}
-
-function reverseGeocode(latlng, callback, markerType) {
-    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latlng.lat}&lon=${latlng.lng}&zoom=18&addressdetails=1`;
-
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            if (data && data.address) {
-                const address = data.display_name;
-                callback(address);
-
-                // Send the geocoded address to the server
-                sendGeocodedAddress(address, markerType);
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
-
-function sendGeocodedAddress(address, markerType) {
-    const url = '/marker-location';
-
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
+    const currentLocationInput = $("#current-location");
+  
+    currentLocationInput.select2({
+      placeholder: "Select current location",
+      ajax: {
+        url: "https://api.opencagedata.com/geocode/v1/json",
+        dataType: "json",
+        delay: 250,
+        data: function (params) {
+          return {
+            q: params.term,
+            key: "YOUR_OPENCAGE_API_KEY",
+          };
         },
-        body: JSON.stringify({ address, markerType }),
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            console.log(`Geocoded address (${markerType}) sent successfully`);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
-
-function setLocation(inputId) {
-    const location = document.getElementById(inputId).value;
-    geocoder.geocode(location, function (results) {
-        if (results.length > 0) {
-            const latlng = results[0].center;
-            if (inputId === "current-location") {
-                currentLocationMarker.setLatLng(latlng).update();
-                map.panTo(latlng);
-            } else if (inputId === "destination") {
-                destinationMarker.setLatLng(latlng).update();
-                map.panTo(latlng);
-            }
-        }
+        processResults: function (data) {
+          const suggestions = data.results.map((result) => ({
+            id: result.geometry.lat + "," + result.geometry.lng,
+            text: result.formatted,
+          }));
+          return {
+            results: suggestions,
+          };
+        },
+        cache: true,
+      },
     });
-}
-
-initMap();
-
-
+  
+    currentLocationInput.on("select2:select", function (e) {
+      const latlng = e.params.data.id.split(",");
+      currentLocationMarker.setLatLng(latlng).update();
+      map.panTo(latlng);
+    });
+  }
+  
+  function updateDestinationMarker() {
+    const destinationInput = $("#destination");
+  
+    destinationInput.select2({
+      placeholder: "Select destination",
+      ajax: {
+        url: "https://api.opencagedata.com/geocode/v1/json",
+        dataType: "json",
+        delay: 250,
+        data: function (params) {
+          return {
+            q: params.term,
+            key: "c986aaa28af74f65b90e997289e37903",
+          };
+        },
+        processResults: function (data) {
+          const suggestions = data.results.map((result) => ({
+            id: result.geometry.lat + "," + result.geometry.lng,
+            text: result.formatted,
+          }));
+          return {
+            results: suggestions,
+          };
+        },
+        cache: true,
+      },
+    });
+  
+    destinationInput.on("select2:select", function (e) {
+      const latlng = e.params.data.id.split(",");
+      destinationMarker.setLatLng(latlng).update();
+      map.panTo(latlng);
+    });
+  }
+  initMap()
--- server.js
+++ server.js
@@ -3,52 +3,33 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST' && req.url === '/marker-location') {
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk;
-    });
-
-    req.on('end', () => {
-      const { address, markerType } = JSON.parse(body);
-      console.log(`Received Marker Location (${markerType}): ${address}`);
-      res.writeHead(200);
-      res.end();
-    });
-  } else {
-    let filePath = '.' + req.url;
-    if (filePath === './') {
-      filePath = './welcome.html'; // Change the default homepage to newRide.html
-    }
-
-    const ext = path.extname(filePath);
-    let contentType = 'text/html';
-    if (ext === '.css') {
-      contentType = 'text/css';
-    } else if (ext === '.js') {
-      contentType = 'text/javascript';
-    }
-
-    fs.readFile(filePath, (err, content) => {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          // File not found
-          res.writeHead(404);
-          res.end('404 Not Found');
-        } else {
-          // Server error
-          res.writeHead(500);
-          res.end('500 Internal Server Error');
-        }
-      } else {
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(content);
-      }
-    });
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './welcome.html';
   }
+
+  const resolvedFilePath = path.resolve(filePath);
+
+  fs.readFile(resolvedFilePath, (err, content) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('404 Not Found');
+    } else {
+      let contentType = 'text/html';
+      if (filePath.endsWith('.css')) {
+        contentType = 'text/css';
+      } else if (filePath.endsWith('.js')) {
+        contentType = 'text/javascript';
+      } else if (filePath.endsWith('.png')) {
+        contentType = 'image/png';
+      }
+
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
 });
 
-const port = 3000; // Change the port number if needed
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
 });
